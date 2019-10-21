925f649efd82caa3de0f13cf84be18a1d68a2538
lounet <54711678+lounet3301@users.noreply.github.com>
2019-10-21 22:46:46 +0530

    s

--- graph-only.py
+++ graph-only.py
@@ -1,7 +1,8 @@
+#Please Run this Command Before running the program
+#pip install -r requirements.txt - windows
+#pip3 install -r requirements.txt - unix based systems
 import pandas as pd
 from matplotlib import pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-root= Tk()
 data = pd.read_csv("MOCK_DATA.csv")
 df = pd.DataFrame(data)
 kick = df[["kick"]]
@@ -12,3 +13,10 @@ d = df[["d"]]
 e = df[["e"]]
 f = df[["f"]]
 g = df[["g"]]
+def normal_plot():
+    plt.plot(kick,a)
+    plt.show()
+def scatter_plot():
+    plt.scatter(kick,a)
+    plt.show()
+scatter_plot()
